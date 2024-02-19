f8f7ce6154f6a02674beaf80e27dec5c342c53af
derik <mzcdata@gmail.com>
2024-02-19 16:15:48 +0900

    commit

--- function_app.py
+++ function_app.py
@@ -1,18 +1,25 @@
 import azure.functions as func
-import datetime
 import json
 import logging
-import time
-from azure.iot.device import IoTHubDeviceClient, Message
+
+from azure.iot.device import IoTHubDeviceClient
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=True,
-              use_monitor=False) 
-def TimerTrigger(myTimer: func.TimerRequest) -> None:
+# Learn more at aka.ms/pythonprogrammingmodel
 
-    # Azure IoT Hub 연결 문자열
-    CONNECTION_STRING = ""
+# Get started by running the following code to create a function using a HTTP trigger.
+
+@app.function_name(name="reservation_http_trigger")
+@app.route(route="reservation_http_trigger")
+def test_function(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    req_body = req.get_json()
+
+    CONNECTION_STRING = req_body.get("CONNECTION_STRING")
+    DEVICE_ID = req_body.get("DEVICE_ID")
+    RESERVATION_METHOD = req_body.get("RESERVATION_METHOD")
 
     # 메시지를 전송할 디바이스 클라이언트 생성
     device_client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
@@ -20,29 +27,24 @@ def TimerTrigger(myTimer: func.TimerRequest) -> None:
     # 연결
     device_client.connect()
 
-    try:
-        # 전송할 메시지 생성
-        message = Message("Hello from Python!")
-
-        # 메시지 속성 설정 (옵션)
-        message.custom_properties["temperature"] = "27.3"
-        message.custom_properties["humidity"] = "60.5"
-
-
-        # 메시지 전송
-        device_client.send_message(message)
-        print("Message sent: {}".format(message))
-
-        # 일정 간격으로 메시지 전송을 반복하려면 sleep을 사용할 수 있습니다.
-        time.sleep(5)
-
-    except KeyboardInterrupt:
-        print("IoTHubClient sample stopped")
-
-    # 연결 종료
+    # 메시지 생성
+    json_message = {
+        "reserve_user_id" : "1",
+        "reserve_device_id" : DEVICE_ID,
+        "reservation_method" : RESERVATION_METHOD, # KIOSK, VIEW, WEB, APP 
+        "reservation_datetime" : "reservation_datetime_string",
+        "reservation_participants" : 3,
+        "current_datetime" : "current_datetime_str"
+    }
+    
+    str_message = json.dumps(json_message)
+    print("Message sent: {}".format(str_message))
+    
+    # 연결 종료9
     device_client.disconnect()
+    logging.info('Python http trigger function executed.')
 
-    if myTimer.past_due:
-        logging.info('The timer is past due!')
+    # 메시지 전송
+    device_client.send_message(str_message)
 
-    logging.info('Python timer trigger function executed.')
+    return func.HttpResponse(f"Hello, {DEVICE_ID}. This HTTP triggered function executed successfully.")
